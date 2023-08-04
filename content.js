3fd1ed01c3d3f8991de2d9080dbf1b39540ec692
Arthur Melton <me@arthurmelton.com>
2023-08-04 00:04:23 -0400

    Get it working

--- content.js
+++ content.js
@@ -1,18 +1,24 @@
 let action = 0;
 
+const options = {"width": 1024, "height": 1024};
+
 document.body.addEventListener("keyup", function(e) {
     if (new Date() - action < 1000) {
-        if (e.ctrlKey && e.shiftKey) {
-            if (e.code = "KeyF") {add(foxes.fox({"width": 1024, "height": 1024}))}
-        }
         action = 0;
+        if (e.ctrlKey && e.shiftKey) {
+            if (e.code == "KeyF") {add(foxes.fox(options))}
+            else if (e.code == "Digit1") {add(foxes.curious(options))}
+            else if (e.code == "Digit2") {add(foxes.happy(options))}
+            else if (e.code == "Digit3") {add(foxes.scary(options))}
+            else if (e.code == "Digit4") {add(foxes.sleeping(options))}
+        }
     }
     else if (e.ctrlKey && e.shiftKey && e.code == "KeyF") {
         action = new Date();
 
         new Promise(r => setTimeout(r, 1000)).then(() => {
             if (action != 0) {
-                add(foxes.fox({"width": 1024, "height": 1024}));
+                add(foxes.fox(options));
             }
         });
     }
